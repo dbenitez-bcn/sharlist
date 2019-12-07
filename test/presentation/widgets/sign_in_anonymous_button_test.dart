c6e9cf380db392f93a13bd3aa984e2625d9e2c90
dbenitez-bcn <dbr.bcn@gmail.com>
2019-12-07 20:59:48 +0100

    test enhanced

--- test/presentation/widgets/sign_in_anonymous_button_test.dart
+++ test/presentation/widgets/sign_in_anonymous_button_test.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sharlist/core/injection_container.dart';
 import 'package:sharlist/domain/usecases/sign_in_anonymously.dart';
+import 'package:sharlist/presentation/bloc/auth/auth_bloc.dart';
 import 'package:sharlist/presentation/widgets/sign_in_anonymous_button.dart';
 
 import '../../core/test_app.dart';
+import '../bloc/auth/auth_bloc_test.dart';
 
 class SignInAnonymouslyMock extends Mock implements SignInAnonymously {
   int called = 0;
@@ -16,6 +19,8 @@ class SignInAnonymouslyMock extends Mock implements SignInAnonymously {
   }
 }
 
+class AuthBlocMock extends AuthBloc {}
+
 void main() {
   setUp(() {
     sl.registerLazySingleton<SignInAnonymously>(() => SignInAnonymouslyMock());
@@ -24,8 +29,13 @@ void main() {
       (WidgetTester tester) async {
     // Arrange
     SignInAnonymouslyButton widget = SignInAnonymouslyButton();
+    BlocProvider provider = BlocProvider<AuthBloc>(
+        builder: (_) => AuthBloc(
+            signInAnonymously: sl<SignInAnonymously>(),
+            signInWithGoogle: MockGoogleSignIn()),
+        child: Center(child: widget));
     // Act
-    await tester.pumpWidget(TestApp(widget));
+    await tester.pumpWidget(TestApp(provider));
     await tester.tap(find.byKey(Key('signInAnonymouslyButton')));
     final SignInAnonymouslyMock useCase = sl<SignInAnonymously>();
     // Assert
